23354c3e9c961ce2e0410cf9a69ffe607b89fba9
Yiya Zhuang <79792524+adaiyaa@users.noreply.github.com>
2021-09-08 00:24:58 +1000

    Construct RegularRobot

--- src/automail/RegularRobot.java
+++ src/automail/RegularRobot.java
@@ -1,4 +1,22 @@
 package automail;
 
-public class RegularRobot {
+import simulation.IMailDelivery;
+
+/** A regular delivery robot **/
+public class RegularRobot extends Robot{
+    {
+        TUBE_CAPACITY = 1;
+    }
+
+    /**
+     * Initiates the robot's location at the start to be at the mailroom
+     * also set it to be waiting for mail.
+     * @param number gives the id of current robot
+     * @param delivery governs the final delivery
+     * @param mailPool is the source of mail items
+     */
+    public RegularRobot(IMailDelivery delivery, MailPool mailPool, int number) {
+        super(delivery, mailPool);
+        this.id = "R" + number;
+    }
 }
